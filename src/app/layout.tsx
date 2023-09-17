10ce10088e5a886f4da7d040257903c8bce1d4c8
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-17 08:35:26 -0700

    Added a styling import and a config override in order to enable font awesome to work properly with next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+// font awesome styles import to enable fa css behaviors in nextjs
+import '@fortawesome/fontawesome-svg-core/styles.css'
+// font awesome config override to disable fa from auto adding <style>
+// elements into the <head> of pages
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
