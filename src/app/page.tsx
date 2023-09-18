573ae8431dc62e676d82e7f200a23ad607e2421a
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-18 15:05:27 -0700

    Added headings to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,23 @@
+import { Heading } from "@/components/Heading/Heading";
 
 export default function Home() {
-  return (<></>)
+  return (
+    <>
+      <Heading
+        level={6}
+        content='Hi, my name is'
+        sx={{fontWeight: 'lighter'}}
+      />
+      <Heading
+        level={1}
+        content='Bryan Wolf'
+        sx={{fontWeight: 'bold'}}
+      />
+      <Heading
+        level={6}
+        content='I love all things tech'
+        sx={{fontWeight: 'lighter'}}
+      />
+    </>
+  )
 }
