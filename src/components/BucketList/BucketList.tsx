ebdbf0a6f27ee7b494b6ff60493eab49dbea5bff
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-19 17:47:36 -0700

    Created BucketList component and created a data file to populate the list

---
+++ src/components/BucketList/BucketList.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React, { useState, useEffect } from "react";
+import bucketList from "@/data/bucketList";
+
+interface BucketListProps {
+    item: string;
+    checked: boolean;
+}
+
+export const BucketList: React.FC = () => {
+    const [list, setList] = useState<BucketListProps[]>([]);
+
+
+    useEffect(() => {
+        setList(bucketList);
+    }, [list])
+
+    const listItems = list.map((item, i) => {
+        return (
+            <label key={i} className='flex pt-5 pb-5' htmlFor={item.item}>
+                <input checked={item.checked} className='self-center rounded-full bg-green-100 border-green-500 text-green-500 focus:ring-green-200 w-5 h-5' type='checkbox' />
+                <span className='self-center ml-2'>{item.item}</span>
+            </label>
+        );
+    });
+
+    return(
+        <>
+            <div className=' grid grid-cols-5'>
+                {listItems}
+            </div>
+        </>
+    );
+};
+
+export default BucketList;
---
+++ src/data/bucketList.ts
@@ -0,0 +1,89 @@
+export const bucketList = [
+    {
+        item: 'Visit all US national parks',
+        checked: false,
+    },
+    {
+        item: 'Visit all AZ state parks',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    {
+        item: '',
+        checked: false,
+    },
+    
+];
+
+export default bucketList;
