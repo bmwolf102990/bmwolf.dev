0e8f4ec31b6448e4d09587aeec0009b6a30a45fb
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-20 12:16:43 -0700

    Replaced bucketList.ts file with bucketListData.ts file

---
+++ src/data/bucketListData.ts
@@ -0,0 +1,129 @@
+type BucketListDataType = {
+    item: string;
+    checked: boolean;
+};
+
+export const bucketListData: BucketListDataType[] = [
+    {
+        item: 'Visit all 7 continents',
+        checked: false,
+    },
+    {
+        item: 'Visit all US national parks',
+        checked: false,
+    },
+    {
+        item: 'Visit all AZ state parks',
+        checked: false,
+    },
+    {
+        item: 'Hike the entire PCT',
+        checked: false,
+    },
+    {
+        item: 'Hike the entire CDT',
+        checked: false,
+    },
+    {
+        item: 'Hike the entire GPT',
+        checked: false,
+    },
+    {
+        item: 'Hike the entire AT',
+        checked: false,
+    },
+    {
+        item: 'Complete the Phoenix Marathon',
+        checked: false,
+    },
+    {
+        item: 'Summit Denali',
+        checked: false,
+    },
+    {
+        item: 'Summit Aconcagua',
+        checked: false,
+    },
+    {
+        item: 'Summit Vinson Massif',
+        checked: false,
+    },
+    {
+        item: 'Summit Mont Blanc',
+        checked: false,
+    },
+    {
+        item: 'Summit Kilimanjaro',
+        checked: false,
+    },
+    {
+        item: 'Summit Elbrus',
+        checked: false,
+    },
+    {
+        item: 'Summit Everest',
+        checked: false,
+    },
+    {
+        item: 'Summit Puncak Jaya',
+        checked: false,
+    },
+    {
+        item: 'Summit Kosciuszko',
+        checked: false,
+    },
+    {
+        item: 'Summit Chimborazo',
+        checked: false,
+    },
+    {
+        item: 'Custom build a gaming computer',
+        checked: false,
+    },
+    {
+        item: 'Custom build a smart mirror',
+        checked: false,
+    },
+    {
+        item: 'Custom build a home server',
+        checked: false,
+    },
+    {
+        item: 'Practice Brazilian Jiu-Jitsu',
+        checked: false,
+    },
+    {
+        item: 'Practice Tai Chi',
+        checked: false,
+    },
+    {
+        item: 'Practice Qigong',
+        checked: false,
+    },
+    {
+        item: 'Practice rock climbing',
+        checked: false,
+    },
+    {
+        item: 'Practice meditation',
+        checked: true,
+    },
+    {
+        item: 'Attend a USWNT match',
+        checked: false,
+    },
+    {
+        item: 'Attend a UNMNT match',
+        checked: false,
+    },
+    {
+        item: 'Attend a Uruguay match',
+        checked: false,
+    },
+    {
+        item: 'Attend a FIFA World Cup',
+        checked: false,
+    },
+];
+
+export default bucketListData;
