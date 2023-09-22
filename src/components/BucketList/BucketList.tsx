537fc1b2a2495620cfa5650a33df24474bd880e6
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-22 07:26:18 -0700

    Updated BucketList component

--- src/components/BucketList/BucketList.tsx
+++ src/components/BucketList/BucketList.tsx
@@ -19,7 +19,7 @@ export const BucketList: React.FC = () => {
     const listItems = list.map((item, i) => {
         return (
             <label key={i} className='flex pt-5 pb-5' htmlFor={item.item}>
-                <input checked={item.checked} className='self-center rounded-full bg-green-100 border-green-500 text-green-500 focus:ring-green-200 w-5 h-5' type='checkbox' />
+                <input disabled checked={item.checked} className='self-center rounded-full bg-green-100 border-green-500 text-green-500 focus:ring-green-200 w-5 h-5' type='checkbox' />
                 <span className='self-center ml-2'>{item.item}</span>
             </label>
         );
