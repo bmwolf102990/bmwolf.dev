f1f4c76972a6e5b7b5521385112ab99ff3a98979
bmwolf102990 <bmwolf.professional@gmail.com>
2023-09-22 07:26:57 -0700

    Updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=''>
-      <section className='bg-white pt-20 pl-20'>
+    <main>
+      <section className='bg-white 4k:pt-40 desktop:pt-40 netbook:pt-40 4k:pl-40 desktop:pl-20 netbook:pl-20 h-[400px]'>
         <Heading
           level={6}
           content='Hi, my name is'
@@ -28,7 +28,7 @@ export default function Home() {
         />
         <div className='curve'></div>
       </section>
-      <section className='bg-stacked_waves_purple bg-cover bg-no-repeat h-[875px] pt-24 pl-20'>
+      <section className='bg-stacked_waves_purple bg-cover bg-no-repeat h-[1575px] 4k:pt-96 desktop:pt-80 netbook:pt-80 4k:pl-40 desktop:pl-20 netbook:pl-20'>
         <Heading
           level={6}
           content='Passion Project'
@@ -39,7 +39,7 @@ export default function Home() {
           content='Digital Ramblings'
           sx={{fontWeight: 'bold', color: 'white'}}
         />
-        <p className='w-1/3 font-thin text-white mb-5'>
+        <p className='w-1/3 font-thin text-lg text-white mb-5'>
           {`Digital Ramblings is a podcast that I am developing with my buddy Anthony. It is a way for us to be creative and have some fun.`}
         </p>
         <Link className='block hover:underline text-white' href='#'>
@@ -47,7 +47,7 @@ export default function Home() {
           <FontAwesomeIcon className='ml-2' icon={faArrowAltCircleRight} />
         </Link>
       </section>
-      <section className='grid grid-cols-2 bg-white h-[300px] pt-16 pl-20'>
+      <section className='grid grid-cols-2 bg-white h-[400px] 4k:pt-40 desktop:pt-20 netbook:pt-0 4k:pl-40 desktop:pl-20 netbook:pl-20'>
         <div>
           <Heading
             level={6}
@@ -59,7 +59,7 @@ export default function Home() {
             content='Banyan Labs & Indeed'
             sx={{fontWeight: 'bold'}}
           />
-          <p className='font-thin mb-5'>
+          <p className='font-thin text-lg mb-5'>
             {`In 2023 I began working through Banyan Labs as a contracted Front End Engineer for Indeed. It has been an unbelievable learning experience and a motivator for me to keep developing my skills.`}
           </p>
           <Link className='block hover:underline' href='https://banyanlabs.io'>
@@ -69,22 +69,22 @@ export default function Home() {
         </div>
         <div>
           <Image
-            className='scale-150 translate-x-24 translate-y-40' 
-            src='/banyan_logo.png'
+            className='scale-150 4k:translate-x-80 desktop:translate-x-40 netbook:translate-x-24 4k:translate-y-40 desktop:translate-y-40 netbook:translate-y-40' 
+            src='/banyan.png'
             alt=''
             width={200}
             height={200}
           />
           <Image
-            className='scale-150 origin-top-right translate-x-80 translate-y-8 rotate-12' 
-            src='/indeed_logo.svg'
+            className='4k:scale-150 desktop:scale-150 netbook:scale-125 origin-left 4k:translate-x-96 desktop:translate-x-32 netbook:translate-x-12 -translate-y-20 rotate-12' 
+            src='/indeed.svg'
             alt=''
             width={300}
             height={300}
           />
         </div>
       </section>
-      <section className='bg-stacked_waves_red bg-cover bg-no-repeat h-[875px] pt-4 pl-20'>
+      <section className='bg-stacked_waves_red bg-cover bg-no-repeat h-[1575px] 4k:pt-96 desktop:pt-60 netbook:pt-48 4k:pl-40 desktop:pl-20 netbook:pl-20'>
         <Heading
           level={6}
           content='Passion Project'
@@ -95,7 +95,7 @@ export default function Home() {
           content='visualDev.io'
           sx={{fontWeight: 'bold', color: 'white'}}
         />
-        <p className='w-1/2 font-thin text-white mb-5'>
+        <p className='w-1/2 font-thin text-lg text-white mb-5'>
           {`Web development learning tools for visual learners.`}
           <br />
           <br />
@@ -106,7 +106,7 @@ export default function Home() {
           <FontAwesomeIcon className='ml-2' icon={faArrowAltCircleRight} />
         </Link>
       </section>
-      <section className='grid grid-cols-2 bg-white h-[300px] pl-20'>
+      <section className='grid grid-cols-2 bg-white h-[400px] 4k:pt-40 desktop:pt-0 netbook:pt-0 4k:pl-40 desktop:pl-20 netbook:pl-20'>
         <div>
           <Heading
             level={6}
@@ -118,7 +118,7 @@ export default function Home() {
             content='Shield Bar & JTC'
             sx={{fontWeight: 'bold'}}
           />
-          <p className='font-thin mb-5'>
+          <p className='font-thin text-lg mb-5'>
             {`I joined Shield Bar Marketing in 2022 as a Website Design Lead. I helped design and develop 15+ websites for clients and helped launch a rebranding and redesign campaign for SBM's flagship site.`}
             <br />
             <br />
@@ -135,22 +135,22 @@ export default function Home() {
         </div>
         <div>
           <Image
-            className='scale-150 translate-x-40 translate-y-10' 
-            src='/sbm_logo.png'
+            className='scale-150 4k:translate-x-96 desktop:translate-x-40 netbook:translate-x-32 4k:-translate-y-12 desktop:-translate-y-12 netbook:translate-y-8' 
+            src='/sbm.png'
             alt=''
             width={200}
             height={200}
           />
           <Image
-            className='scale-150 origin-top-right translate-x-60 translate-y-16 rotate-225' 
-            src='/jtc_logo.jpeg'
+            className='4k:scale-150 desktop:scale-150 netbook:scale-150 desktop:origin-left netbook:origin-right 4k:translate-x-96 desktop:translate-x-48 netbook:translate-x-32 4k:translate-y-0 desktop:translate-y-0 netbook:translate-y-32 rotate-12' 
+            src='/jtc.jpeg'
             alt=''
             width={300}
             height={300}
           />
         </div>
       </section>
-      <section className='bg-stacked_waves_green bg-cover bg-no-repeat h-[875px] pt-20 pl-20'>
+      <section className='bg-stacked_waves_green bg-cover bg-no-repeat h-[1575px] 4k:pt-96 desktop:pt-80 netbook:pt-80 4k:pl-40 desktop:pl-20 netbook:pl-20'>
         <Heading
           level={6}
           content='Open Source'
@@ -161,7 +161,7 @@ export default function Home() {
           content='LinksHub & Milan'
           sx={{fontWeight: 'bold', color: 'white'}}
         />
-        <p className='w-1/2 font-thin text-white mb-5'>
+        <p className='w-1/2 font-thin text-lg text-white mb-5'>
           {`Collaboration is the heart and soul of the internet. So in that spirit, I contribute to the development of open source projects like LinksHub and Milan.`}
         </p>
         <Link className='block hover:underline text-white mb-2' href='https://linkshub.dev'>
@@ -173,7 +173,7 @@ export default function Home() {
           <FontAwesomeIcon className='ml-2' icon={faArrowAltCircleRight} />
         </Link>
       </section>
-      <section className='grid grid-cols-2 bg-white h-[300px] pt-24 pl-20'>
+      <section className='grid grid-cols-2 bg-white h-[400px] 4k:pt-40 desktop:pt-20 netbook:pt-20 4k:pl-40 desktop:pl-20 netbook:pl-20'>
         <div>
           <Heading
             level={6}
@@ -185,7 +185,7 @@ export default function Home() {
             content='Persevere Now'
             sx={{fontWeight: 'bold'}}
           />
-          <p className='font-thin mb-5'>
+          <p className='font-thin text-lg mb-5'>
             {`I started my journey into software engineering and web development with the help of an organization known as Persevere Now. Simply put, they changed my life forever.`}
           </p>
           <Link className='block hover:underline' href='https://perseverenow.org'>
@@ -195,34 +195,34 @@ export default function Home() {
         </div>
         <div>
           <Image
-            className='scale-150 translate-x-20 translate-y-40 -rotate-12' 
-            src='/persevere_logo.png'
+            className='4k:scale-150 desktop:scale-150 netbook:scale-100 4k:translate-x-80 desktop:translate-x-32 netbook:translate-x-0 4k:translate-y-40 desktop:translate-y-24 netbook:translate-y-24 -rotate-12' 
+            src='/persevere.png'
             alt=''
             width={400}
             height={400}
           />
         </div>
       </section>
-      <section className='bg-stacked_waves_orange bg-cover bg-no-repeat h-[930px] pt-24 pl-20'>
+      <section className='bg-stacked_waves_orange bg-cover bg-no-repeat h-[1675px] 4k:pt-96 desktop:pt-80 netbook:pt-80 4k:pl-40 desktop:pl-20 netbook:pl-20'>
         <Heading
           level={6}
           content='Blogging'
-          sx={{paddingTop: '25rem', fontWeight: 'bold', color: 'white'}}
+          sx={{paddingTop: '35rem', fontWeight: 'bold', color: 'white'}}
         />
         <Heading 
           level={3}
           content='Medium'
           sx={{fontWeight: 'bold', color: 'white'}}
         />
-        <p className='w-1/2 font-thin text-white mb-5'>
-          {`I recently began putting some of my thoughts into words. Not sure if anyone actually reads any of it, but I do it for me, not for reads.`}
+        <p className='w-1/2 font-thin text-lg text-white mb-5'>
+          {`I recently began putting some of my thoughts into words using the platform Medium. Not sure if anyone actually reads any of it, but I do it for me, not for reads.`}
         </p>
         <Link className='block hover:underline text-white' href='#'>
           Check it out
           <FontAwesomeIcon className='ml-2' icon={faArrowAltCircleRight} />
         </Link>
       </section>
-      <section className='bg-white h-[800px] pt-12 pl-20 pr-20'>
+      <section className='bg-white h-[800px] 4k:pt-20 desktop:pt-0 netbook:pt-0 4k:pl-40 desktop:pl-20 netbook:pl-20 4k:pr-40 desktop:pr-20 netbook:pr-20'>
         <Heading
           level={3}
           content='Bucket List'
@@ -235,10 +235,10 @@ export default function Home() {
         />
         <BucketList />
       </section>
-      <section className='bg-stacked_waves_blue bg-cover bg-no-repeat h-[850px] pt-12 pl-20 pr-20'>
+      <section className='bg-stacked_waves_blue bg-cover bg-no-repeat h-[1575px] pt-96 pl-40'>
     
       </section>
-      <section className='bg-white h-[875px] pl-20 pr-20'>
+      <section className='bg-white h-[975px] 4k:pl-40 desktop:pl-20 netbook:pl-20 4k:pr-40 desktop:pr-20 netbook:pr-20'>
         <Heading
           level={3}
           content='Connect with me'
@@ -246,7 +246,7 @@ export default function Home() {
         />
         <Heading 
           level={6}
-          content='Only if you REALLY want to. No pressure'
+          content='Or just cyberstalk me secretly. Either way, I am flattered'
           sx={{fontWeight: 'lighter', marginBottom: '0.75rem'}}
         />
         <ConnectList />    
@@ -255,7 +255,7 @@ export default function Home() {
         <div className='text-center'>
           <span>designed & developed with </span> 
           <FontAwesomeIcon icon={faHeart} /> 
-          <span> by <a className='underline' href='https://github.com/bmwolf102990'>Bryan Wolf</a></span>
+          <span> by <a className='underline' href='https://github.com/bmwolf102990'>bryan wolf</a></span>
           <br />
           <FontAwesomeIcon icon={faCopyright} />
           <span> 2023</span>
